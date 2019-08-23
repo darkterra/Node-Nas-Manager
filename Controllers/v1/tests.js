ee9edfb3e16da092bf058d794e5dfb71dd8f4fb5
darkterra <darkterra01@gmail.com>
2019-08-23 02:17:07 +0200

    Fix generate filesystem

--- Controllers/v1/tests.js
+++ Controllers/v1/tests.js
@@ -3,6 +3,7 @@
 const { join } = require('path');
 const Datastore = require('nedb');
 const moment = require('moment');
+const fs = require('fs');
 
 const db = {};
 db.fileSystem = new Datastore({ filename: join(__dirname, '..', '..', '..', 'BDD', 'fileSystem.db'), autoload: true });
